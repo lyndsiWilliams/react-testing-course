d463170098279452afc3db5a929cf3c75976624a
lyndsiWilliams <kcatgirl@gmail.com>
2021-02-08 13:26:09 -0600

    Section 5.3 complete

--- react-testing/src/App.test.tsx
+++ react-testing/src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('When everything is OK', () => {
+  it('should render the App component without crashing', () => {
+    render(<App />);
+    screen.debug();
+  });
+
+  it('should select the children that are being passed to the CustomInput component', () => {
+    render(<App />);
+    screen.getByText(/Input/);
+  });
 });
